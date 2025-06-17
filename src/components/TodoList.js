9486e51cc0704321e540ef7da88adb0cd7017f72
AnmolMandhan <151067526+AnmolMandhan@users.noreply.github.com>
2025-06-17 16:18:03 +0500

    TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,1 +1,11 @@
+import React from "react";
+
+export default function TodoItem({ task, toggleComplete, deleteTask }) {
+  return (
+    <div className={`todo-item ${task.completed ? "done" : ""}`}>
+      <span onClick={() => toggleComplete(task.id)}>{task.text}</span>
+      <button onClick={() => deleteTask(task.id)}>❌</button>
+    </div>
+  );
+}
 
